ef4e8c2a0101576c751d0e9f237c45bb1ebb5092
nakayama-yq0117 <78580421+nakayama-yq0117@users.noreply.github.com>
2021-03-31 13:32:18 +0900

    Add files via upload

--- reply.js
+++ reply.js
@@ -5,40 +5,40 @@ $(function () {
   // 送信
   $('form').submit(function () {
     // 有給
-    var check = $('input[name="check"').val()
+    var check = $('input[name="check"]').val()
     // 遅刻
-    var check1 = $('input[name="check1"').val()
-    var time1 = $('input[name="time1"').val()
+    var check1 = $('input[name="check1"]').val()
+    var time1 = $('input[name="time1"]').val()
     // 早退
-    var check2 = $('input[name="check2"').val()
-    var time2 = $('input[name="time2"').val()
+    var check2 = $('input[name="check2"]').val()
+    var time2 = $('input[name="time2"]').val()
     // 訪問先
       // 訪問先１
       // お客様名１
       var cname1 = $('input[name="cname1"]').val();
       var cname1ch1 = $('input[name="cname1-ch1"]').val();
       var cname1ch2 = $('input[name="cname1-ch2"]').val();
-      var cname1text = $('input[name="cname1-text"]').val();
+      var cname1text = $('textarea[name="cname1-text"]').val();
       // お客様名２
       var cname2 = $('input[name="cname2"]').val();
       var cname2ch1 = $('input[name="cname2-ch1"]').val();
       var cname2ch2 = $('input[name="cname2-ch2"]').val();
-      var cname2text = $('input[name="cname2-text"]').val();
+      var cname2text = $('textarea[name="cname2-text"]').val();
       // お客様名３
       var cname3 = $('input[name="cname3"]').val();
       var cname3ch1 = $('input[name="cname3-ch1"]').val();
       var cname3ch2 = $('input[name="cname3-ch2"]').val();
-      var cname3text = $('input[name="cname3-text"]').val();
+      var cname3text = $('textarea[name="cname3-text"]').val();
       // お客様名４
       var cname4 = $('input[name="cname4"]').val();
       var cname4ch1 = $('input[name="cname4-ch1"]').val();
       var cname4ch2 = $('input[name="cname4-ch2"]').val();
-      var cname4text = $('input[name="cname4-text"]').val();
+      var cname4text = $('textarea[name="cname4-text"]').val();
       // お客様名５
       var cname5 = $('input[name="cname5"]').val();
       var cname5ch1 = $('input[name="cname5-ch1"]').val();
       var cname5ch2 = $('input[name="cname5-ch2"]').val();
-      var cname5text = $('input[name="cname5-text"]').val();
+      var cname5text = $('textarea[name="cname5-text"]').val();
     // 苦情
     var replytext = $('textarea[name="reply-text"]').val();
     // 持ち出し
